9edaa8842e6afc27ec0f9a0fbdb8e6b51fda2524
test <test@tesutonoMacBook-Air.local>
2021-01-08 11:11:21 +0900

    add:sorceryを使ったユーザー登録&ログイン機能

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,22 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      flash[:success] = 'ログインしました'
+      redirect_back_or_to root_path
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_path
+  end
+end
