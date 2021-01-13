db7e9583f9ec7fb74494691aa988514ce4380bbd
test <test@tesutonoMacBook-Air.local>
2021-01-13 18:44:57 +0900

    finish:ユーザー登録なしの決断機能

---
+++ app/controllers/guests/choices_controller.rb
@@ -0,0 +1,42 @@
+class Guests::ChoicesController < ApplicationController
+  before_action :set_choice_instance, only: [:confirm, :result]
+  skip_before_action :require_login
+
+  def new
+    @choice = Choice.new
+  end
+
+  def confirm
+    @choice.user_id = 1
+    render :new if @choice.invalid?
+  end
+
+  def result
+    @choice.user_id = 1
+    options = []
+    options.push(@choice.option_1, @choice.option_2, @choice.option_3, @choice.option_4, @choice.option_5)
+    options.reject!(&:blank?)
+    @choice.result = options.sample
+    if params[:back]
+      render :new
+    end
+  end
+
+  def edit
+    render :new
+  end
+
+  def goodbye
+    redirect_to root_path
+  end
+
+  private
+
+  def set_choice_instance
+    @choice = Choice.new(choice_params)
+  end
+
+  def choice_params
+    params.require(:choice).permit(:title, :option_1, :option_2, :option_3, :option_4, :option_5, :result)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :guests do
+    get 'choices/new'
+    get 'choices/create'
+    get 'choices/edit'
+  end
   root 'static_pages#top'
   get '/about', to: 'static_pages#about'
   get '/policy', to: 'static_pages#policy'
@@ -15,6 +20,15 @@ Rails.application.routes.draw do
       get :alert
     end
   end
+
+  namespace :guests do
+    resources :choices, only: [:new, :create, :edit] do
+      collection do
+        post :confirm, :result
+      end
+    end
+  end
+
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
